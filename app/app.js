d39ba7535c7b531fcde87a67b97afdb7a0471ea1
gmaralan <gmartinialan@gmail.com>
2023-03-04 15:44:07 -0400

    refactoring: decided to use JS only method of handling xlsx creation. Removed aditional code to reflect that change.

--- app/app.js
+++ app/app.js
@@ -1,42 +1,13 @@
 /* eslint-disable max-lines-per-function */
 const express = require('express');
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
 const XLSX = require('xlsx');
-const { asyncSpawn } = require('./utils/asyncSpawn');
 const { getInformationFromHTMLString } = require('./handleHtmlString');
 
 const app = express();
 
-app.get('/fiscalnote/:url', async (req, res) => {
-  console.time('test');
-  const fiscalNoteCode = req.params.url;
-  
-  const URL = `http://www.sefaz.mt.gov.br/nfce/consultanfce?p=${fiscalNoteCode}`;
-  const result = await fetch(URL);
-  const text = await result.text();
-
-  const fiscalNoteInfo = getInformationFromHTMLString(text);
-
-  await asyncSpawn(
-    'python3',
-    ['./register_informations.py', JSON.stringify(fiscalNoteInfo)],
-  );
-
-  res.download('./nota_fiscal.xlsx', (err) => {
-    if (err) {
-      console.error('Falha na parte de download:', err);
-
-      res.status(400).send('Failed to download file:', err);
-    }
-
-    fs.unlinkSync('./nota_fiscal.xlsx');
-  });
-
-  console.timeEnd('test')
-});
-
-app.get('/xlsx/:url', async (req, res) => {
+app.get('/generateTable/:url', async (req, res) => {
   console.time('xlsx');
   const fiscalNoteCode = req.params.url;
   
@@ -48,7 +19,9 @@ app.get('/xlsx/:url', async (req, res) => {
   fiscalNoteInfo.unshift(['Itens', 'Valor']);
 
   const workbook = XLSX.utils.book_new();
+
   const worksheet = XLSX.utils.aoa_to_sheet(fiscalNoteInfo);
+  worksheet['!cols'] = [{ width: 60 }, { width: 10 }];
 
   XLSX.utils.book_append_sheet(workbook, worksheet);
 
