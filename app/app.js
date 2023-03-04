789877f5c12be8d4725d28429863f3c33d628d89
gmaralan <gmartinialan@gmail.com>
2023-03-04 14:34:54 -0400

    feat: created python file to manage xlsx table

--- app/app.js
+++ app/app.js
@@ -1,18 +1,30 @@
 const express = require('express');
+const cd = require('child_process');
 const { getInformationFromHTMLString } = require('./handleHtmlString');
 
 const app = express();
 
+app.get('/teste', async (req, res) => {
+  const coolJSON = [[1, 2, 3], ['a', 'b', 'c']];
+  res.send('finished');
+});
+
 app.get('/fiscalnote/:url', async (req, res) => {
-  const fiscalNote = req.params.url;
+  const fiscalNoteCode = req.params.url;
   
-  const URL = `http://www.sefaz.mt.gov.br/nfce/consultanfce?p=${fiscalNote}`;
+  const URL = `http://www.sefaz.mt.gov.br/nfce/consultanfce?p=${fiscalNoteCode}`;
   const result = await fetch(URL);
   const text = await result.text();
 
-  getInformationFromHTMLString(text);
+  const fiscalNoteInfo = getInformationFromHTMLString(text);
 
-  res.send('Finished');
+  cd.spawn(
+    'python3',
+    ['./register_informations.py', JSON.stringify(fiscalNoteInfo)],
+    { stdio: 'inherit' },
+  );
+
+  res.download('./nota_fiscal.xlsx');
 });
 
 module.exports = app;
---
+++ app/register_informations.py
@@ -0,0 +1,21 @@
+import openpyxl as opx
+import os
+import json
+import sys
+
+
+fiscal_information = json.loads(sys.argv[1])
+
+# script_path = os.path.dirname(__file__)
+
+ws = opx.Workbook()
+wa = ws.active
+wa.column_dimensions['A'].width = 60
+wa['A1'].value = 'Nome dos itens'
+wa['B1'].value = 'Preço'
+
+for i, information in enumerate(fiscal_information):
+    wa[f'A{i + 2}'].value = information[0]
+    wa[f'B{i + 2}'].value = information[1]
+
+ws.save('nota_fiscal.xlsx')
